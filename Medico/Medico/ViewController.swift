39b325c0e8f5f901f910bfa30bf76bbd336a93e8
hassayouneM <86298263+hassayouneM@users.noreply.github.com>
2022-03-30 11:47:08 +0100

    Add files via upload
    project created

---
+++ Medico/Medico/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Medico
+//
+//  Created by iMac on 30/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
